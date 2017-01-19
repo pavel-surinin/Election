966ea59ea5635438230d699bb921e092330232c4
pavel-surinin <pavel.surinin@gmail.com>
2017-01-19 19:07:50 +0200

    added login ui

--- src/main/resources/static/js/Containers/LoginContainer.js
+++ src/main/resources/static/js/Containers/LoginContainer.js
@@ -3,17 +3,41 @@ var LoginContainer = React.createClass({
     return {
       username : '',
       password : '',
+      wrongAuth : false,
+      isLogged : false,
     };
+  },
+  componentWillMount: function() {
+    var self = this;
+    axios
+    .get('users/logged')
+    .then(function(response){
+      console.log(response.data);
+      if (response.data == 'none') {
+        self.setState({isLogged : false});
+      } else {
+        self.setState({isLogged : true});
+      }
+    })
+    .catch(function(err){
+      console.error('LoginContainer.componentWillMount.axios',err);
+    });
   },
   onHandleSubmit : function(event){
     event.preventDefault();
     var self = this;
     console.log(this.state);
     axios
-    .post('users/login', self.state)
+    .post('users/login', {
+      username : self.state.username,
+      password : self.state.password,
+    })
     .then(function(response){
-      window.App.call;
-      self.context.router.push('/county');
+      if (response.data) {
+        self.context.router.push('/county');
+      } else {
+        self.setState({wrongAuth : true});
+      }
       console.log(response.data);
 
     })
@@ -39,7 +63,17 @@ var LoginContainer = React.createClass({
   onHandlePasswordChange : function(event){
     this.setState({password: event.target.value});
   },
-
+  onHandleLogoutClick : function(){
+    var self = this;
+    axios
+    .get('/users/logout')
+    .then(function(response) {
+      self.setState({isLogged : false});
+    })
+    .catch(function(err){
+      console.error('LoginContainer.onHandleLogoutClick.axios',err);
+    });
+  },
   render: function() {
     return (
       <LoginComponent
@@ -47,6 +81,9 @@ var LoginContainer = React.createClass({
       onHandlePasswordChange={this.onHandlePasswordChange}
       onHandleUsernameChange={this.onHandleUsernameChange}
       userLogged={this.checkWhoIsLogged}
+      wrongAuth={this.state.wrongAuth}
+      isLogged={this.state.isLogged}
+      onHandleLogoutClick={this.onHandleLogoutClick}
       />
     );
   }
