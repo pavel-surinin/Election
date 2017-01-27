3b8afe2c0db2351d7374a9645082ae348d32293d
Alxemic <Alxemic>
2017-01-27 09:41:51 +0200

    Before pull

--- src/main/resources/static/js/Containers/PartyCreateContainer.js
+++ src/main/resources/static/js/Containers/PartyCreateContainer.js
@@ -1,10 +1,9 @@
-
 var PartyCreateContainer = React.createClass({
   getInitialState: function() {
     return {
       name : '',
       nameClone : false,
-      nameErrorMesages : [],
+    
     };
   },
   componentWillMount: function() {
@@ -14,27 +13,20 @@ var PartyCreateContainer = React.createClass({
     this.setState({name : event.target.value});
   },
   onHandleSubmit : function(event){
-    event.preventDefault();
     var self = this;
-    //validation
-    var nameErrorMesages = [];
-    if(!validation.checkPartyName(this.state.name)) {nameErrorMesages.push('Pavadinimą gali sudaryti tik raidės ir tarpai');}
-    if(!validation.checkMax(this.state.name,50)) {nameErrorMesages.push('Pavadinimas negali būti ilgesnis, nei 50 simbolių');}
-    if(!validation.checkMin(this.state.name,4)) {nameErrorMesages.push('Pavadinimas negali būti trumpesnis, nei 4 simboliai');}
-    if (nameErrorMesages.length == 0) {
-      var partyName = {name: this.state.name.trim()};
-      axios
+    event.preventDefault();
+    var partyName = {name: this.state.name.trim()};
+    var successAlertText = ('Partija sukurta');
+    axios
       .post('/party', partyName)
       .then(function(response){
+        console.log(response);
         self.context.router.push('/admin/party');
       })
       .catch(function(err){
         console.error('PartyCreateContainer.onHandleSubmit.axios', err);
         self.setState({nameClone : true});
       });
-    } else {
-      this.setState({nameErrorMesages : nameErrorMesages});
-    }
   },
   render: function() {
     return (
@@ -44,7 +36,6 @@ var PartyCreateContainer = React.createClass({
        onHandleSubmit={this.onHandleSubmit}
        nameClone={this.state.nameClone}
        action='Sukurti'
-       nameErrorMesages={this.state.nameErrorMesages}
        />
     );
   }
