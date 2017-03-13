94af9427dd505475453c3b2b58e69bdcd4a8e8cc
Gabriele <gabriele.sieliunaite@gmail.com>
2017-03-13 12:40:28 +0200

    CandidateServiceTest

---
+++ src/test/java/lt/itakademija/electors/candidate/CandidateServiceTest.java
@@ -0,0 +1,239 @@
+package lt.itakademija.electors.candidate;
+
+import lt.itakademija.electors.county.CountyEntity;
+import lt.itakademija.electors.county.CountyRepository;
+import lt.itakademija.electors.district.DistrictEntity;
+import lt.itakademija.electors.district.DistrictService;
+import lt.itakademija.electors.party.PartyEntity;
+import lt.itakademija.electors.party.PartyRepository;
+import lt.itakademija.electors.party.PartyService;
+import lt.itakademija.electors.results.single.ResultSingleRepository;
+import lt.itakademija.electors.results.single.ResultSingleService;
+import org.hamcrest.CoreMatchers;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.MockitoRule;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by Gabriele on 2017-03-13.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class CandidateServiceTest {
+
+    @Mock
+    private CandidateService candidateService;
+    @Mock
+    private CandidateRepository candidateRepository;
+    @Mock
+    private PartyService partyService;
+    @Mock
+    private PartyRepository partyRepository;
+    @Mock
+    private CountyRepository countyRepository;
+    @Mock
+    private DistrictService districtService;
+    @Mock
+    private ResultSingleService resultSingleService;
+    @Mock
+    private ResultSingleRepository resultSingleRepository;
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    @Before
+    public void setUp() throws Exception {
+        PartyEntity party1 = Mockito.mock(PartyEntity.class);
+        CountyEntity county1 = Mockito.mock(CountyEntity.class);
+        CandidateEntity candidate1 = Mockito.mock(CandidateEntity.class);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        candidateRepository.getCandidatesList().stream().forEach(c -> candidateService.delete(c.getId()));
+        partyRepository.findAll().stream().forEach(p -> partyService.delete(p.getId()));
+    }
+
+    @Test
+    public void save() throws Exception {
+        CandidateEntity candidate3 = Mockito.mock(CandidateEntity.class);
+        candidateRepository.save(candidate3);
+        when(candidateRepository.save(candidate3)).thenReturn(candidate3);
+        Mockito.verify(candidateRepository).save(candidate3);
+        assertEquals(candidateRepository.save(candidate3), candidate3);
+    }
+
+    @Test
+    public void getAllCandidates() throws Exception {
+
+        CandidateEntity candidate1 = Mockito.mock(CandidateEntity.class);
+        CandidateEntity candidate2 = Mockito.mock(CandidateEntity.class);
+        List <CandidateReport> candidatesList = new LinkedList();
+        List spy = Mockito.spy(candidatesList);
+
+        spy.add(candidate1);
+        spy.add(candidate2);
+
+        Mockito.verify(spy).add(candidate1);
+        Mockito.verify(spy).add(candidate2);
+
+        assertEquals(2, spy.size());
+    }
+
+    @Test
+    public void deleteCandidatesByPartyId() throws Exception {
+
+        PartyEntity party1 = Mockito.mock(PartyEntity.class);
+        when(party1.getId()).thenReturn(6L);
+        PartyEntity party2 = Mockito.mock(PartyEntity.class);
+        when(party2.getId()).thenReturn(76L);
+        CandidateEntity candidate1 = Mockito.mock(CandidateEntity.class);
+        when(candidate1.getPartyDependencies()).thenReturn(party1);
+        CandidateEntity candidate2 = Mockito.mock(CandidateEntity.class);
+        when(candidate2.getPartyDependencies()).thenReturn(party2);
+        CandidateEntity candidate3 = Mockito.mock(CandidateEntity.class);
+        when(candidate3.getPartyDependencies()).thenReturn(party1);
+
+        List <CandidateEntity> candidatesList = new LinkedList();
+        List spy = Mockito.spy(candidatesList);
+        spy.add(candidate1);
+        spy.add(candidate2);
+        spy.add(candidate3);
+        Mockito.verify(spy).add(candidate1);
+        Mockito.verify(spy).add(candidate2);
+        Mockito.verify(spy).add(candidate3);
+
+        System.out.print(candidatesList);
+        candidateService.deleteCandidatesByPartyId(76L);
+        when(candidateService.deleteCandidatesByPartyId(76L)).thenReturn(true);
+        Mockito.verify(candidateService).deleteCandidatesByPartyId(76L);
+        assertEquals(candidateService.deleteCandidatesByPartyId(76L), true);
+
+    }
+
+    @Test
+    public void getCandidateByNameSurnameNumberParty() throws Exception {
+
+    }
+
+    @Test
+    public void getCandidateById() throws Exception {
+        CandidateEntity candidate1 = Mockito.mock(CandidateEntity.class);
+        when(candidate1.getId()).thenReturn(34L);
+        CandidateEntity candidate2 = Mockito.mock(CandidateEntity.class);
+        when(candidate2.getId()).thenReturn(23L);
+
+        when(candidateRepository.finById(23L)).thenReturn(candidate2);
+        assertThat(candidateRepository.finById(23L), CoreMatchers.is(candidate2));
+    }
+
+    @Test
+    public void getCandidateByDistrictId() throws Exception {
+        CountyEntity county1 = Mockito.mock(CountyEntity.class);
+        when(county1.getId()).thenReturn(3L);
+        CountyEntity county2 = Mockito.mock(CountyEntity.class);
+        when(county2.getId()).thenReturn(29L);
+
+        DistrictEntity district1 = Mockito.mock(DistrictEntity.class);
+        when(district1.getId()).thenReturn(12L);
+        when(district1.getCounty()).thenReturn(county1);
+
+        DistrictEntity district2 = Mockito.mock(DistrictEntity.class);
+        when(district2.getId()).thenReturn(24L);
+        when(district2.getCounty()).thenReturn(county2);
+
+        CandidateEntity candidate1 = Mockito.mock(CandidateEntity.class);
+        when(candidate1.getCounty()).thenReturn(county1);
+        CandidateEntity candidate2 = Mockito.mock(CandidateEntity.class);
+        when(candidate2.getCounty()).thenReturn(county2);
+
+        List <CandidateReport> candidatesListDistrict1 = new LinkedList();
+        List spyCounty1 = Mockito.spy(candidatesListDistrict1);
+        spyCounty1.add(candidate1);
+        Mockito.verify(spyCounty1).add(candidate1);
+
+        List <CandidateReport> candidatesListDistrict2 = new LinkedList<>();
+        List spyCounty2 = Mockito.spy(candidatesListDistrict2);
+        spyCounty2.add(candidate2);
+        Mockito.verify(spyCounty2).add(candidate2);
+
+        candidateService.getCandidateByDistrictId(24L);
+        when(candidateService.getCandidateByDistrictId(24L)).thenReturn(spyCounty2);
+        Mockito.verify(candidateService).getCandidateByDistrictId(24L);
+        assertEquals(candidateService.getCandidateByDistrictId(24L), spyCounty2);
+
+        candidateService.getCandidateByDistrictId(12L);
+        when(candidateService.getCandidateByDistrictId(12L)).thenReturn(spyCounty1);
+        Mockito.verify(candidateService).getCandidateByDistrictId(12L);
+        assertEquals(candidateService.getCandidateByDistrictId(12L), spyCounty1);
+    }
+
+    @Test
+    public void getCandidatesByCounty() throws Exception {
+
+        CountyEntity county1 = Mockito.mock(CountyEntity.class);
+        CountyEntity county2 = Mockito.mock(CountyEntity.class);
+        CandidateEntity candidate1 = Mockito.mock(CandidateEntity.class);
+        when(candidate1.getCounty()).thenReturn(county1);
+        CandidateEntity candidate2 = Mockito.mock(CandidateEntity.class);
+        when(candidate2.getCounty()).thenReturn(county2);
+
+        List <CandidateEntity> candidatesListCounty1 = new LinkedList();
+        List spyCounty1 = Mockito.spy(candidatesListCounty1);
+        spyCounty1.add(candidate1);
+        Mockito.verify(spyCounty1).add(candidate1);
+
+        List <CandidateEntity> candidatesListCounty2 = new LinkedList();
+        List spyCounty2 = Mockito.spy(candidatesListCounty2);
+        spyCounty2.add(candidate2);
+        Mockito.verify(spyCounty2).add(candidate2);
+
+        when(candidateRepository.findByCounty(county1)).thenReturn(spyCounty1);
+        assertThat(candidateRepository.findByCounty(county1), CoreMatchers.is(spyCounty1));
+    }
+
+    @Test
+    public void deleteAsObject() throws Exception {
+        PartyEntity party2 = Mockito.mock(PartyEntity.class);
+        when(party2.getId()).thenReturn(55L);
+
+        CandidateEntity candidate2 = Mockito.mock(CandidateEntity.class);
+        when(candidate2.getId()).thenReturn(2L);
+        when(candidate2.getPartyDependencies()).thenReturn(party2);
+
+        assertThat(candidate2.getId(), CoreMatchers.is(2L));
+        assertThat(candidate2.getPartyDependencies().getId(), CoreMatchers.is(55L));
+
+        when(candidateRepository.delete(candidate2)).thenReturn(true);
+        assertThat(candidateRepository.delete(candidate2), CoreMatchers.is(true));
+    }
+
+    @Test
+    public void deleteById() throws Exception {
+        PartyEntity party2 = Mockito.mock(PartyEntity.class);
+        when(party2.getId()).thenReturn(55L);
+
+        CandidateEntity candidate2 = Mockito.mock(CandidateEntity.class);
+        when(candidate2.getId()).thenReturn(2L);
+        when(candidate2.getPartyDependencies()).thenReturn(party2);
+
+        assertThat(candidate2.getId(), CoreMatchers.is(2L));
+        assertThat(candidate2.getPartyDependencies().getId(), CoreMatchers.is(55L));
+
+        when(candidateRepository.delete(candidate2.getId())).thenReturn(true);
+        assertThat(candidateRepository.delete(candidate2.getId()), CoreMatchers.is(true));
+    }
+
+}
