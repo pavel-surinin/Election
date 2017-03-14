3f7b37b7b8ad94a61a291455961588ec6cfb068b
Gabriele <gabriele.sieliunaite@gmail.com>
2017-03-14 13:38:07 +0200

    party tests started

---
+++ src/test/java/lt/itakademija/electors/party/PartyServiceTest.java
@@ -0,0 +1,94 @@
+package lt.itakademija.electors.party;
+
+import lt.itakademija.electors.county.CountyService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.MockitoRule;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Gabriele on 2017-03-14.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class PartyServiceTest {
+
+    @Mock
+    private CountyService countyService;
+
+    @Mock
+    private PartyService partyService;
+
+    @Mock
+    private PartyRepository partyRepository;
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void save() throws Exception {
+
+    }
+
+    @Test
+    public void save1() throws Exception {
+
+    }
+
+    @Test
+    public void save2() throws Exception {
+
+    }
+
+    @Test
+    public void getPartyByNumber() throws Exception {
+
+    }
+
+    @Test
+    public void getPartyList() throws Exception {
+
+    }
+
+    @Test
+    public void getPartyById() throws Exception {
+
+    }
+
+    @Test
+    public void getPartyEntityById() throws Exception {
+
+    }
+
+    @Test
+    public void detach() throws Exception {
+
+    }
+
+    @Test
+    public void delete() throws Exception {
+
+    }
+
+    @Test
+    public void getNameById() throws Exception {
+
+    }
+
+}
